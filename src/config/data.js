cab059b052f06c18b967d43c45de187dfda4fa17
Chahatg01 <81353884+Chahatg01@users.noreply.github.com>
2022-11-21 23:41:34 +0530

    Add files via upload

---
+++ src/config/data.js
@@ -0,0 +1,87 @@
+export const blogList = [
+    {
+      id: 1,
+      title: '7 CSS tools you should be using ',
+      category: 'development',
+      subCategory: ['frontend', 'ui/ux', 'design'],
+      description:
+        "CSS tools are very popular among graphic designers and web designers. Many tools have been introduced recently because the website loading speed has greatly been attributed to the CSS tools and the CSS generators. 7 CSS tools are CSS menu maker, ScoutApp, Compass, Bourbon, CSS Beautifier, CSS Shrink, Rapid CSS editor",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/designer-1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Milan Places That Highlight The City',
+      category: 'travel',
+      subCategory: ['vacation', 'holidays', 'sight seeing'],
+      description:
+        "Start your 3-hour tour at the Milan Cathedral. Your entrance ticket, provided by your guide, enables you to skip the lines to visit the church. Inside, admire its baptistery and enjoy rooftop views.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/f67396fc3cfce63a28e07ebb35d974ac.jpg',
+    },
+    {
+      id: 3,
+      title: 'Online Shopping – An Alternative to Shopping in the Mall',
+      category: 'shopping',
+      subCategory: ['e-commerce store', 'clothing', 'shopping store'],
+      description:
+        "Online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser or a mobile app. Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, which displays the same product's availability and pricing at different e-retailers.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/fQwuyKJ9qxjSbr6REcgtmW-1200-80.jpg',
+    },
+    {
+      id: 4,
+      title: 'ADVENTURE IN YOU',
+      category: 'adventure',
+      subCategory: ['adrenaline', 'stay-fit', 'lifestyle'],
+      description:
+        "An adventure is an exciting experience or undertaking that is typically bold, sometimes risky.[1] Adventures may be activities with danger such as traveling, exploring, skydiving, mountain climbing, scuba diving, river rafting, or other extreme sports. Adventures are often undertaken to create psychological arousal or in order to achieve a greater goal, such as the pursuit of knowledge that can only be obtained by such activities.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/adventure.webp',
+    },
+    {
+      id: 5,
+      title: 'Loaded BBQ Baked Potatoes',
+      category: 'cooking',
+      subCategory: ['bbq', 'food', 'lifestyle'],
+      description:
+        "A baked potato, known in some parts of the United Kingdom (though not generally Scotland) as a jacket potato,[1] is a preparation of potato. It may be served with fillings, toppings or condiments such as butter, cheese, sour cream, gravy, baked beans, and even ground meat or corned beef.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/make-it-personal.jpg',
+    },
+    {
+      id: 6,
+      title: 'Beyond the Beach',
+      category: 'travel',
+      subCategory: ['beaches', 'sea', 'holidays'],
+      description:
+        "A beach is a landform alongside a body of water which consists of loose particles. The particles composing a beach are typically made from rock, such as sand, gravel, shingle, pebbles, etc., or biological sources, such as mollusc shells or coralline algae.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover:
+        '/assets/images/Purple-Combination-colors-graphic-design-predictions-1024x576-1024x576.jpg',
+    },
+    {
+      id: 7,
+      title: 'Art & Perception',
+      category: 'art',
+      subCategory: ['skill', 'design', 'passion'],
+      description:
+        "Art is a diverse range of human activity, and resulting product, that involves creative or imaginative talent expressive of technical proficiency, beauty.Perception in art stands for a complex relation between visual stimuli and a personal understanding of them. It is a theoretical postulate that aims to clarify the relation between artworks and individual opinions and evaluations.",
+      authorName: 'John Doe',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2021',
+      cover: '/assets/images/Synthwave-Postmodern.jpg',
+    },
+  ];
